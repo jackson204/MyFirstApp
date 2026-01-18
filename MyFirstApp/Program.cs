533367cfae90ad7b050419d8ba10568fef6b7bfe
jackson <temp@example.com>
2026-01-18 16:29:06 +0800

    update Program.cs to enhance response handling with custom headers and HTML content

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -5,6 +5,8 @@ var app = builder.Build();
 app.Run(async context =>
 {
     context.Response.StatusCode = 400;
-    await context.Response.WriteAsync("Hello World!");
+    context.Response.Headers.Append("mykey", "value");
+    context.Response.Headers.Append("Content-Type", "text/html; charset=utf-8");
+    await context.Response.WriteAsync("<h1>Hello World!</h1>");
 });
 app.Run();
