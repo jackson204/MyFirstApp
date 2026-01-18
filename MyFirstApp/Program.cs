4b6a6c84846c630f4d1d301758283c8f99914dae
jackson <temp@example.com>
2026-01-18 15:51:03 +0800

    update Program.cs to change response handling for root endpoint

--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -2,6 +2,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
-app.Map("/", () => "Hello, World!");
-
+app.Run(async context =>
+{
+    context.Response.StatusCode = 400;
+    await context.Response.WriteAsync("Hello World!");
+});
 app.Run();
